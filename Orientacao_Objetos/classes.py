4ae9ee2dd0a9bd4e797c1df057d1c32a5f038510
Gustavo Fontenele <gustavoafo1711@gmail.com>
2022-06-28 15:20:27 -0300

    classes

---
+++ Orientacao_Objetos/classes.py
@@ -0,0 +1,29 @@
+"""
+POO - Classes
+
+Classes: nada mais são do que modelos dos objetos do mundo real sendo representados computacionalmente.
+
+Imagine que você queira fazer um sistema para automatizar o controle das lâmpadas da sua casa.
+
+Classes podem conter:
+    - Atributos -> representam as características do objeto. Ou seja, pelos atributos conoseguimos
+    representar computacionalmente os estados de um objeto. No caso da lâmpdda, possivelmente
+    iríamos querer saber se a lâmpada é 110 ou 220 volts, se ela é branca, amarela, vermelha,
+    qual é a luminosidade dela, etc.
+
+    - Métodos (funções) -> representam os comportamentos do objetos, ou seja, as ações que este
+    objeto pode realizar no seu sistema. No caso da lâmpada, por exemplo, um comportamento comum
+    que muito provavelmente iríamos querer representar no nosso sistema é o de ligar e desligar.
+
+Em Python para definir uma classe, utilizamos a palavrea reservada class.
+
+"""
+
+
+class Lampada:
+    pass
+
+
+lamp = Lampada()
+print(type(lamp))
+
