7c1b19c07d4258991bb2fd166cd3569632b0181e
Gustavo Fontenele <gustavoafo1711@gmail.com>
2022-06-29 11:37:56 -0300

    classes

--- Orientacao_Objetos/atributos.py
+++ Orientacao_Objetos/atributos.py
@@ -31,6 +31,80 @@ public class Lampada(){
     }
 }
 
+
+
+# obs: lembre-se que isso é apenas uma convenção, a linguagem Python não
+# vai impedir que façamos acesso aos atributos sinalizados como privados fora da classe
+
+
+# Exemplo
+
+
+user = Acesso('user@gmail.com', '123456')
+
+print(f'Usuário: {user.email}')
+# print(f'Senha: {user.__senha}')  # AttributeError
+
+print(f'Usuário: {user._Acesso__senha}')  # Não recomendado acessar dessa maneira
+user.mostra_senha()
+
+
+
+# O que significa atributos de instância?
+
+# Significa que ao criarmos instâncias/objetos de uma classe, todas as instâncias terão
+# estes atributos.
+
+
+user1 = Acesso('user1@gamail.com', '123456')
+user2 = Acesso('user2@gamail.com', '987654')
+
+user1.mostra_email()
+user2.mostra_email()
+
+
+
+# Atributos de Classe
+
+
+# Atributos de classe são atributos que são declarados diretamente na classe,
+# ou seja, fora do construtor. Geralmente já inicializamos um valor, e este
+# valor é compartilhado entre todas as instâncias da classe, ou seja, ao invés
+# de cada intância ter seus próprios valores como é o caso dos atributos de
+# instância, com os atributos de classe todas as instâncias terão o mesmo valor
+# para este atributo.
+
+# Refatorar a classe Produto
+
+
+class Produto:
+
+    # Atributo de classe
+    imposto = 1.05  # 0.05% de imposto
+    contador = 0
+
+    def __init__(self, nome, descricao, valor):
+        self.id = Produto.contador + 1
+        self.nome = nome
+        self.descricao = descricao
+        self.valor = (valor * Produto.imposto)
+        Produto.contador = self.id
+
+
+p1 = Produto('Playstation', 'Video game', 2500)
+p2 = Produto('Atari', 'Video game', 1345)
+
+print(p1.valor)
+print(p2.valor)
+
+# obs: não precisamos criar uma instância de uma classe para fazer acesso a um atributo de classe
+print(f'Acessando imposto sem criar instância: acesso correto atributo de classe (Produto.imposto) {Produto.imposto}')
+print(f'Acessando imposto com instância: acesso incorreto (p1.imposto) {p1.imposto}')
+
+
+print(f'\nid do p1: {p1.id}')
+print(f'id do p2: {p2.id}')
+
 """
 
 
@@ -78,20 +152,38 @@ class Acesso:
     def mostra_senha(self):
         print(self.__senha)
 
-
-# obs: lembre-se que isso é apenas uma convenção, a linguagem Python não
-# vai impedir que façamos acesso aos atributos sinalizados como privados fora da classe
-
-
-# Exemplo
+    def mostra_email(self):
+        print(self.email)
 
 
-user = Acesso('user@gmail.com', '123456')
+# Atributos Dinâmicos -> um atributo de instância que pode ser criado em tempo de execução
 
-print(f'Usuário: {user.email}')
-# print(f'Senha: {user.__senha}')  # AttributeError
+# obs: o atributo dinâmico será exclusivo da instÂncia que o criou.
 
-print(f'Usuário: {user._Acesso__senha}')  # Não recomendado acessar dessa maneira
-user.mostra_senha()
+p1 = Produto('Playstation', 'Video Game', 2500)
+p2 = Produto('Arroz', 'Mercearia', 5.99)
 
+# Criando um atributo dinâmico em tempo de execução
+p2.peso = '5Kg'  # na classe Produto não exite peso
 
+print(f'Produto: {p2.nome}\nDescrição: {p2.descricao}\nValor: {p2.valor}\nPeso: {p2.peso}')
+
+# print(f'Produto: {p1.nome}\nDescrição: {p1.descricao}\nValor: {p1.valor}\nPeso: {p1.peso}') #AttributeError
+
+# Deletando atributos
+
+print('\n')
+print(p1.__dict__)
+print(p2.__dict__)
+
+del p2.peso
+print(p1.__dict__)
+print(p2.__dict__)
+
+del p2.valor
+print(p1.__dict__)
+print(p2.__dict__)
+
+del p2.descricao
+print(p1.__dict__)
+print(p2.__dict__)
